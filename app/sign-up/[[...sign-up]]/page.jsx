542bbdb7feadd4956727af8e13e1f0f1dec9ab11
Muhammad Faheem <malikfaheem.007001@gmail.com>
2026-07-18 10:32:56 +0500

    Noindex sign up alias page

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,4 +1,11 @@
 import AuthForm from "@/components/AuthForm";
+import { buildNoIndexMetadata } from "@/lib/seo";
+
+export const metadata = buildNoIndexMetadata({
+  title: "Sign Up",
+  description: "Create your StudyAI account.",
+  path: "/sign-up",
+});
 
 export default function SignUpAliasPage() {
   return <AuthForm mode="signup" />;
