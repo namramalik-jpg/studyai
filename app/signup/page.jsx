e5e1965a7cb01c5579c7e3aa727967d93f28b7cb
Muhammad Faheem <malikfaheem.007001@gmail.com>
2026-07-18 10:32:57 +0500

    Noindex signup page

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,4 +1,11 @@
 import AuthForm from "@/components/AuthForm";
+import { buildNoIndexMetadata } from "@/lib/seo";
+
+export const metadata = buildNoIndexMetadata({
+  title: "Sign Up",
+  description: "Create your free StudyAI account.",
+  path: "/signup",
+});
 
 export default function SignupPage() {
   return <AuthForm mode="signup" />;
